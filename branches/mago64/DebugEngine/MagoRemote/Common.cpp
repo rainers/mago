f2b0cee23cda57724c9a709ca4c4fbf40553a52e
aldon <aldon@8a68931a-4790-44fb-9eda-ef479ae8b71b>
2014-01-05 18:15:43 +0000

    Add the MagoRemote project. This is the remote agent.

---
+++ branches/mago64/DebugEngine/MagoRemote/Common.cpp
@@ -0,0 +1,5 @@
+// Common.cpp : source file that includes just the standard includes
+// MagoRemote.pch will be the pre-compiled header
+// Common.obj will contain the pre-compiled type information
+
+#include "Common.h"
---
+++ branches/mago64/DebugEngine/MagoRemote/MagoRemote.cpp
@@ -0,0 +1,22 @@
+/*
+   Copyright (c) 2013 Aldo J. Nunez
+
+   Licensed under the Apache License, Version 2.0.
+   See the LICENSE text file for details.
+*/
+
+// MagoRemote.cpp : Defines the entry point for the application.
+//
+
+#include "Common.h"
+#include "MagoRemote.h"
+
+
+int APIENTRY wWinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPTSTR    lpCmdLine,
+    int       nCmdShow )
+{
+    return 0;
+}
