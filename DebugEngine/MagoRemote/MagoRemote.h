6ced7d7d1e40e9d68c094cc2196c201b00ae39a2
aldon <aldon@8a68931a-4790-44fb-9eda-ef479ae8b71b>
2014-01-05 18:15:43 +0000

    Add the MagoRemote project. This is the remote agent.

---
+++ DebugEngine/MagoRemote/MagoRemote.h
@@ -0,0 +1,10 @@
+/*
+   Copyright (c) 2013 Aldo J. Nunez
+
+   Licensed under the Apache License, Version 2.0.
+   See the LICENSE text file for details.
+*/
+
+#pragma once
+
+#include "resource.h"
