9619839b819abc44b7c53e04a5509a1c548d9b63
aldon <aldon@8a68931a-4790-44fb-9eda-ef479ae8b71b>
2014-01-13 01:19:44 +0000

    Once a debugging session's opened in exclusive mode (as in local 64-bit debugging), ignore the timer. And, only shut down the remote agent after the session is closed.

--- branches/mago64/DebugEngine/MagoRemote/App.cpp
+++ branches/mago64/DebugEngine/MagoRemote/App.cpp
@@ -13,9 +13,15 @@
 
 enum
 {
+    WM_MAGO_SESSION_ADDED = WM_USER + 1,
+    WM_MAGO_SESSION_DELETED = WM_USER + 2,
+
     ConnectTimeoutMillis = AgentStartupTimeoutMillis,
     ConnectTimeoutErrorCode = -1,
 };
+
+UINT32 gMainTid;
+UINT32 gStartedExclusiveSession;
 
 
 namespace Mago
@@ -46,7 +52,11 @@ namespace Mago
     {
         HRESULT     hr = S_OK;
         MSG         msg;
+        int         sessionCount = 0;
         UINT_PTR    timerId = 0;
+
+        // Make these available for as soon as the server starts listening for calls
+        gMainTid = GetCurrentThreadId();
 
         hr = InitSingleSessionLoop( sessionUuidStr );
         if ( FAILED( hr ) )
@@ -68,7 +78,25 @@ namespace Mago
                 case WM_TIMER:
                     KillTimer( NULL, timerId );
                     timerId = 0;
-                    PostQuitMessage( ConnectTimeoutErrorCode );
+                    if ( NewSession() )
+                    {
+                        // Because NewSession just claimed the only debugging session, it means 
+                        // that no other debugging session was opened, no other session can be 
+                        // opened, and time ran out. So, quit.
+                        PostQuitMessage( ConnectTimeoutErrorCode );
+                    }
+                    break;
+
+                case WM_MAGO_SESSION_ADDED:
+                    sessionCount++;
+                    break;
+
+                case WM_MAGO_SESSION_DELETED:
+                    sessionCount--;
+                    if ( sessionCount == 0 )
+                    {
+                        PostQuitMessage( 0 );
+                    }
                     break;
                 }
             }
@@ -80,5 +108,22 @@ namespace Mago
         UninitRpcServer();
 
         return msg.wParam;
+    }
+
+    void NotifyAddSession()
+    {
+        PostThreadMessage( gMainTid, WM_MAGO_SESSION_ADDED, 0, 0 );
+    }
+
+    void NotifyRemoveSession()
+    {
+        PostThreadMessage( gMainTid, WM_MAGO_SESSION_DELETED, 0, 0 );
+    }
+
+    bool NewSession()
+    {
+        // Allow only one debugging session to be opened
+        UINT32 oldVal = InterlockedExchange( &gStartedExclusiveSession, 1 );
+        return oldVal == 0;
     }
 }
