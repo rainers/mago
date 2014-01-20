f7b5bc33ebf86fd270cb64f426bd115c8b3030b2
aldon <aldon@8a68931a-4790-44fb-9eda-ef479ae8b71b>
2014-01-20 18:33:25 +0000

    Define some more status codes, if they're not defined already in WinNT.h by the current platform SDK.

--- DebugEngine/MagoNatDE/RpcUtil.cpp
+++ DebugEngine/MagoNatDE/RpcUtil.cpp
@@ -22,27 +22,47 @@ void __RPC_USER midl_user_free( void* p )
     free( p );
 }
 
+// The documentation for RpcExceptionFilter says that it handles STATUS_POSSIBLE_DEADLOCK,
+// STATUS_INSTRUCTION_MISALIGNMENT, and STATUS_HANDLE_NOT_CLOSABLE. But, these three macros 
+// are defined in ntstatus.h instead of WinNT.h, which is the usual header file. Use their 
+// values here.
+
+#if !defined( STATUS_POSSIBLE_DEADLOCK )
+#define STATUS_POSSIBLE_DEADLOCK            0xC0000194
+#endif
+
+#if !defined( STATUS_INSTRUCTION_MISALIGNMENT )
+#define STATUS_INSTRUCTION_MISALIGNMENT     0xC00000AA
+#endif
+
+#if !defined( STATUS_HANDLE_NOT_CLOSABLE )
+#define STATUS_HANDLE_NOT_CLOSABLE          0xC0000235
+#endif
+
+#if !defined( STATUS_STACK_BUFFER_OVERRUN )
+#define STATUS_STACK_BUFFER_OVERRUN         0xC0000409
+#endif
+
+#if !defined( STATUS_ASSERTION_FAILURE )
+#define STATUS_ASSERTION_FAILURE            0xC0000420
+#endif
+
 // This function is modeled after RpcExceptionFilter declared in rpcdce.h, which is only available 
 // in Windows Vista and later.
 
 int RPC_ENTRY CommonRpcExceptionFilter( unsigned long exceptionCode )
 {
-    // The documentation for RpcExceptionFilter says that it handles STATUS_POSSIBLE_DEADLOCK,
-    // STATUS_INSTRUCTION_MISALIGNMENT, and STATUS_HANDLE_NOT_CLOSABLE. But, these three macros 
-    // are defined in ntstatus.h instead of WinNT.h, which is the usual header file. Use their 
-    // values here.
-
     switch ( exceptionCode )
     {
     case STATUS_ACCESS_VIOLATION:
-    case 0xC0000194:                    // STATUS_POSSIBLE_DEADLOCK
-    case 0xC00000AA:                    // STATUS_INSTRUCTION_MISALIGNMENT
+    case STATUS_POSSIBLE_DEADLOCK:
+    case STATUS_INSTRUCTION_MISALIGNMENT:
     case STATUS_DATATYPE_MISALIGNMENT:
     case STATUS_PRIVILEGED_INSTRUCTION:
     case STATUS_ILLEGAL_INSTRUCTION:
     case STATUS_BREAKPOINT:
     case STATUS_STACK_OVERFLOW:
-    case 0xC0000235:                    // STATUS_HANDLE_NOT_CLOSABLE
+    case STATUS_HANDLE_NOT_CLOSABLE:
     case STATUS_IN_PAGE_ERROR:
     case STATUS_ASSERTION_FAILURE:
     case STATUS_STACK_BUFFER_OVERRUN:
